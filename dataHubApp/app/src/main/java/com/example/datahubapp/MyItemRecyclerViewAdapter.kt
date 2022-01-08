3c8825ab1be1d7cea364063eb53d2e6b48e82891
ilkimo <vibertikim@yahoo.it>
2022-01-08 19:11:26 +0100

    fix: list placeholder

--- dataHubApp/app/src/main/java/com/example/datahubapp/MyItemRecyclerViewAdapter.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/MyItemRecyclerViewAdapter.kt
@@ -5,9 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.example.datahubapp.databinding.ActivityMainBinding
 
 import com.example.datahubapp.placeholder.PlaceholderContent.PlaceholderItem
 import com.example.datahubapp.databinding.ItemTopicBinding
+import java.security.AccessController.getContext
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
@@ -31,18 +33,18 @@ class MyItemRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
+        holder.idView.text = "Topic ${item.id}"
+        //holder.contentView.text = item.content
     }
 
     override fun getItemCount(): Int = values.size
 
     inner class ViewHolder(binding: ItemTopicBinding) : RecyclerView.ViewHolder(binding.root) {
         val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
+        //val contentView: TextView = binding.content
 
         override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
+            return super.toString() + " '" + idView.text + "'"
         }
     }
 
--- dataHubApp/app/src/main/java/com/example/datahubapp/placeholder/PlaceholderContent.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/placeholder/PlaceholderContent.kt
@@ -21,7 +21,7 @@ object PlaceholderContent {
      */
     val ITEM_MAP: MutableMap<String, PlaceholderItem> = HashMap()
 
-    private val COUNT = 25
+    private val COUNT = 50
 
     init {
         // Add some sample items.
@@ -36,7 +36,7 @@ object PlaceholderContent {
     }
 
     private fun createPlaceholderItem(position: Int): PlaceholderItem {
-        return PlaceholderItem(position.toString(), "Item " + position, makeDetails(position))
+        return PlaceholderItem(position.toString(), "Item $position", makeDetails(position))
     }
 
     private fun makeDetails(position: Int): String {
