23a70e0a8dbf15af92fd3e931ba14f5183e520d2
ilkimo <vibertikim@yahoo.it>
2022-01-08 14:52:51 +0100

    refactor: layout files fragment_topics, item_topic

--- dataHubApp/app/src/main/java/com/example/datahubapp/MyItemRecyclerViewAdapter.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/MyItemRecyclerViewAdapter.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 import com.example.datahubapp.placeholder.PlaceholderContent.PlaceholderItem
-import com.example.datahubapp.databinding.FragmentTopicsBinding
+import com.example.datahubapp.databinding.ItemTopicBinding
 
 /**
  * [RecyclerView.Adapter] that can display a [PlaceholderItem].
@@ -20,7 +20,7 @@ class MyItemRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
         return ViewHolder(
-            FragmentTopicsBinding.inflate(
+            ItemTopicBinding.inflate(
                 LayoutInflater.from(parent.context),
                 parent,
                 false
@@ -37,7 +37,7 @@ class MyItemRecyclerViewAdapter(
 
     override fun getItemCount(): Int = values.size
 
-    inner class ViewHolder(binding: FragmentTopicsBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(binding: ItemTopicBinding) : RecyclerView.ViewHolder(binding.root) {
         val idView: TextView = binding.itemNumber
         val contentView: TextView = binding.content
 
--- dataHubApp/app/src/main/java/com/example/datahubapp/TopicsFragment.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/TopicsFragment.kt
@@ -29,7 +29,7 @@ class TopicsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_topics_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_topics, container, false)
 
         // Set the adapter
         if (view is RecyclerView) {
