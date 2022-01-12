f9eb065914d7c41c4a11f82e4d8e7ddb1a225b39
ilkimo <vibertikim@yahoo.it>
2022-01-12 16:34:42 +0100

    chore: add fakeLogin data

--- dataHubApp/app/src/main/java/com/example/datahubapp/controller/AppController.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/controller/AppController.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import com.example.datahubapp.data.model.*
+import com.example.datahubapp.data.model.classicData.DoubleData
 import com.example.datahubapp.data.model.classicData.IntegerData
 import com.example.datahubapp.data.model.classicData.StringData
 import com.example.datahubapp.data.viewmodel.AppViewModel
@@ -47,35 +48,98 @@ class AppController(val model: AppViewModel, val repository: Repository) {
             nameType.add(parameter1)
             nameType.add(parameter2)
 
-            val descrizioni = arrayOf("descrizione uno", "descrizione due", "descrizione tre")
-            val misurazioni_valori = intArrayOf(10, 20, 30)
+            val descrizioni = arrayOf("descrizione uno", "descrizione due", "descrizione tre", "descrizione quattro", "descrizione cinque")
+            val misurazioni_valori = intArrayOf(10, 20, 30, 40, 50)
 
             val listaCampiRegistrati1 = ArrayList<SourceDataInterface<*>>()
             listaCampiRegistrati1.add(StringData(descrizioni[0]))
             listaCampiRegistrati1.add(IntegerData(misurazioni_valori[0]))
-            val registration1 = Registration(1L, LocalDate.now(), listaCampiRegistrati1)
+            val registration1 = Registration(1L, LocalDate.ofYearDay(2022, 1), listaCampiRegistrati1)
 
             val listaCampiRegistrati2 = ArrayList<SourceDataInterface<*>>()
             listaCampiRegistrati2.add(StringData(descrizioni[1]))
             listaCampiRegistrati2.add(IntegerData(misurazioni_valori[1]))
-            val registration2 = Registration(2L, LocalDate.now(), listaCampiRegistrati2)
+            val registration2 = Registration(2L, LocalDate.ofYearDay(2022, 2), listaCampiRegistrati2)
 
             val listaCampiRegistrati3 = ArrayList<SourceDataInterface<*>>()
             listaCampiRegistrati3.add(StringData(descrizioni[2]))
             listaCampiRegistrati3.add(IntegerData(misurazioni_valori[2]))
-            val registration3 = Registration(3L, LocalDate.now(), listaCampiRegistrati3)
+            val registration3 = Registration(3L, LocalDate.ofYearDay(2022, 3), listaCampiRegistrati3)
+
+
+            val listaCampiRegistrati4 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati4.add(StringData(descrizioni[2]))
+            listaCampiRegistrati4.add(IntegerData(misurazioni_valori[2]))
+            val registration4 = Registration(4L, LocalDate.ofYearDay(2022, 4), listaCampiRegistrati4)
+
+
+            val listaCampiRegistrati5 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati5.add(StringData(descrizioni[2]))
+            listaCampiRegistrati5.add(IntegerData(misurazioni_valori[2]))
+            val registration5 = Registration(5L, LocalDate.ofYearDay(2022, 5), listaCampiRegistrati5)
 
             val registrazioni_box = ArrayList<Registration>()
             registrazioni_box.add(registration1)
             registrazioni_box.add(registration2)
             registrazioni_box.add(registration3)
+            registrazioni_box.add(registration4)
+            registrazioni_box.add(registration5)
+
+            val nameType_peso = ArrayList<DataInfoPair>().apply { add(DataInfoPair("peso", "Double")) }
+
+            val listaCampiRegistrati_peso1 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso1.add(DoubleData(70.0))
+            val registration_peso1 = Registration(1L, LocalDate.ofYearDay(2022, 1), listaCampiRegistrati_peso1)
+
+            val listaCampiRegistrati_peso2 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso2.add(DoubleData(69.5))
+            val registration_peso2 = Registration(2L, LocalDate.ofYearDay(2022, 2), listaCampiRegistrati_peso2)
+
+            val listaCampiRegistrati_peso3 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso3.add(DoubleData(70.1))
+            val registration_peso3 = Registration(3L, LocalDate.ofYearDay(2022, 3), listaCampiRegistrati_peso3)
+
+            val listaCampiRegistrati_peso4 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso4.add(DoubleData(70.2))
+            val registration_peso4 = Registration(4L, LocalDate.ofYearDay(2022, 4), listaCampiRegistrati_peso4)
+
+            val listaCampiRegistrati_peso5 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso5.add(DoubleData(69.7))
+            val registration_peso5 = Registration(5L, LocalDate.ofYearDay(2022, 5), listaCampiRegistrati_peso5)
+
+            val listaCampiRegistrati_peso6 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso6.add(DoubleData(70.3))
+            val registration_peso6 = Registration(6L, LocalDate.ofYearDay(2022, 6), listaCampiRegistrati_peso6)
+
+            val listaCampiRegistrati_peso7 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso7.add(DoubleData(70.1))
+            val registration_peso7 = Registration(7L, LocalDate.ofYearDay(2022, 7), listaCampiRegistrati_peso7)
+
+            val listaCampiRegistrati_peso8 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso8.add(DoubleData(70.1))
+            val registration_peso8 = Registration(8L, LocalDate.ofYearDay(2022, 8), listaCampiRegistrati_peso8)
+
+            val listaCampiRegistrati_peso9 = ArrayList<SourceDataInterface<*>>()
+            listaCampiRegistrati_peso9.add(DoubleData(69.9))
+            val registration_peso9 = Registration(9L, LocalDate.ofYearDay(2022, 9), listaCampiRegistrati_peso9)
+
+            val registrazioni_peso = ArrayList<Registration>()
+            registrazioni_peso.add(registration_peso1)
+            registrazioni_peso.add(registration_peso2)
+            registrazioni_peso.add(registration_peso3)
+            registrazioni_peso.add(registration_peso4)
+            registrazioni_peso.add(registration_peso5)
+            registrazioni_peso.add(registration_peso6)
+            registrazioni_peso.add(registration_peso7)
+            registrazioni_peso.add(registration_peso8)
+            registrazioni_peso.add(registration_peso9)
 
             val topic_box = Topic(
                 1L, "Allenamenti box",  "topic description", LocalDate.now(),
-                colors, registrazioni_box, nameType, 3L, false
+                colors, registrazioni_box, nameType, 8L, false
             )
-            val topic2 = Topic("Peso Corporeo", "topic description", colors, nameType, false)
-            val topic3 = Topic("Sessioni di Studio", "topic description", colors, nameType, false)
+            val topic2 = Topic(2L, "Peso Corporeo", "topic description", LocalDate.now(), colors, registrazioni_peso, nameType, 9L, false)
+            val topic3 = Topic("Sessioni di Studio", "topic description", colors, nameType_peso, false)
             val topic4 = Topic("Spese vacanze", "topic description", colors, nameType, false)
             val topic5 = Topic("Spese alimentari", "topic description", colors, nameType, false)
             val topic6 = Topic("Entrate economiche", "topic description", colors, nameType, false)
