4af89af6df89e0b7fce378e81ee8183c6a4fef9c
ilkimo <vibertikim@yahoo.it>
2022-01-14 17:32:10 +0100

    chore: add edit block on ViewHolders

--- dataHubApp/app/src/main/java/com/example/datahubapp/SelectedRegistrationRecyclerViewAdapter.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/SelectedRegistrationRecyclerViewAdapter.kt
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.TextView
 import com.example.datahubapp.data.model.DataInfoPair
 import com.example.datahubapp.data.model.SourceDataInterface
@@ -190,6 +191,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         val name: TextView = binding.name
         val contentView: CheckBox = binding.content
 
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
+
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
         }
@@ -200,6 +206,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val name: TextView = binding.name
         val contentView: TextView = binding.content
+
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
 
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
@@ -212,6 +223,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         val name: TextView = binding.name
         val contentView: TextView = binding.content
 
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
+
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
         }
@@ -222,6 +238,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val name: TextView = binding.name
         val contentView: TextView = binding.content
+
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
 
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
@@ -234,6 +255,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         val name: TextView = binding.name
         val contentView: TextView = binding.content
 
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
+
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
         }
@@ -243,6 +269,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val name: TextView = binding.name
         val contentView: TextView = binding.content
+
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
 
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
@@ -254,6 +285,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         val name: TextView = binding.name
         val contentView: TextView = binding.content
 
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
+
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
         }
@@ -264,6 +300,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         val name: TextView = binding.name
         val contentView: TextView = binding.content
 
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
+
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
         }
@@ -273,6 +314,11 @@ class SelectedRegistrationRecyclerViewAdapter(
         RecyclerView.ViewHolder(binding.root) {
         val name: TextView = binding.name
         val contentView: TextView = binding.content
+
+        init {
+            //disable editing from GUI
+            contentView.keyListener = null
+        }
 
         override fun toString(): String {
             return super.toString() + " '" + contentView.text + "'"
