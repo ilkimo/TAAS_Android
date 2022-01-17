f83cd802ffb7d620cabdf02fac980d4e3f4a638c
Livio Scarpinati <livio.scarpinati@gmail.com>
2022-01-17 21:53:55 +0100

    chore: change appbar title settings fragment

--- dataHubApp/app/src/main/java/com/example/datahubapp/SettingsFragment.kt
+++ dataHubApp/app/src/main/java/com/example/datahubapp/SettingsFragment.kt
@@ -1,11 +1,19 @@
 package com.example.datahubapp
 
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity?)?.supportActionBar?.title = getString(R.string.settings)
     }
 }
